43feb381f3ca59e353adc96009a4d28c95e926fa
Brenda Vieira <brendanascimentovieirah@gmail.com>
2023-08-25 13:41:28 -0400

    commit

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -2,11 +2,33 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ToDo() {
-   
+
+    const[atividade, setAtiviades] = useState("");
+    const[lista, setLista] = useState([]);
+
+    const salvar = (e) => {
+        e.preventDefault();
+        setLista([...lista,{
+            atividade
+        }])
+    }
+
     return (
         <div>
             <h1>Lista de Atividades</h1>
             <Link to="/">home</Link>
+
+        <p>{atividade}</p>
+
+            <form onSubmit={salvar}>
+
+                <input value ={atividade}
+                    onChange={(e)=> setAtividade(e.target.value)} />
+
+                <button>ADD</button>
+            </form>
+           
+           {lista.map((ativ)=> <p>{ativ.atividade}</p>)}
            
         </div>
     );
