8388719714766fb9a6029caa49d3e9812373b0c6
MarcelinoGarcia-NA <marcelinovitorgarcia@gmail.com>
2023-08-25 09:38:07 -0300

    aula 05 criando uma lista

---
+++ src/Todo/ToDo.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function ToDo() {
+   
+    return (
+        <div>
+            <h1>Lista de Atividades</h1>
+            <Link to="/">home</Link>
+           
+        </div>
+    );
+}
